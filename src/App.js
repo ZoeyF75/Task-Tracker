d37f4166530edf761219690223f43fb51bdf4f33
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-03-01 22:54:22 -0800

    react-router-dom routes implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import Footer from './components/Footer';
+import About from './components/About';
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -80,20 +82,34 @@ const App = () => {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask
-        (!showAddTask)} showAdd={showAddTask} /> 
-      {/* shorter version of ternary if showAddTask then show AddTask component */}
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? 
-      <Tasks 
-        tasks={tasks}
-        onDelete={deleteTask}
-        onToggle={toggleReminder}
-       /> 
-       : 'No Tasks To Show' }
-      <Footer />
-    </div>
+    <Router>
+      <div className="container">
+        <Header 
+          onAdd={() => setShowAddTask(!showAddTask)} 
+          showAdd={showAddTask} /> 
+
+        <Route 
+          path='/' 
+          exact 
+          render={(props) => (
+          <>
+            {/* shorter version of ternary if showAddTask then show AddTask component */}
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (
+            <Tasks 
+              tasks={tasks}
+              onDelete={deleteTask}
+              onToggle={toggleReminder}
+            /> 
+          ) : ('No Tasks To Show' 
+          )}
+          </>
+        )} />
+
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
   );
 } 
 
