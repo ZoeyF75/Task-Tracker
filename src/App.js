54ed36b2d95c32b80ce93980f257b7b9e946f115
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-03-01 20:59:34 -0800

    Added toggle for addTask form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -49,8 +50,10 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header /> 
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask
+        (!showAddTask)} showAdd={showAddTask} /> 
+      {/* shorter version of ternary if showAddTask then show AddTask component */}
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? 
       <Tasks 
         tasks={tasks}
