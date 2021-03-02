6326283d0b2eb547830d8219ea9945cfbe08cc00
Zoey Farago <zoeyfarago@Zoeys-MacBook-Pro.local>
2021-03-01 22:26:11 -0800

    setup JSON Server, fake api frontend, intro of Footer component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "Feb 6th at 1:30pm",
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
     }
-  ]);
+    getTasks();
+  }, []);
+
+  //fetch return promise for Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
 
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    //new task = generated id and everything in task object from onAdd in onSubmit
-    const newTask = {id, ...task};
-    //copies tasks that are already there and adds new task...similar to append in JQuery Tweeter Project
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', 
+    {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+    const data = await res.json();
+    setTasks([...tasks, data]);
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // //new task = generated id and everything in task object from onAdd in onSubmit
+    // const newTask = {id, ...task};
+    // //copies tasks that are already there and adds new task...similar to append in JQuery Tweeter Project
+    // setTasks([...tasks, newTask]);
   }
 
   //Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`,
+     {
+       method: 'DELETE'
+     })
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
   //Toggle Reminder
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder
+    }
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, 
+    {
+      method: 'PUT',
+      headers: {
+        'Content-type' : 'application.json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = await res.json();
     setTasks(
       tasks.map((task) => 
       task.id === id ? {...task, reminder: !task.reminder} : task)
@@ -61,6 +92,7 @@ const App = () => {
         onToggle={toggleReminder}
        /> 
        : 'No Tasks To Show' }
+      <Footer />
     </div>
   );
 } 
